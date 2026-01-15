1aa99b8246f1b36246bcfcfe95f1fe500eb190d9
Akhil Madipalli <99692811+akhilmadipalli@users.noreply.github.com>
2026-01-15 10:59:07 -0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from functions import pdf_to_str_arr, print_fem, print_masc
-
-if __name__ == "__main__":
-    resume = pdf_to_str_arr(input("Type in your resume's relative path: "))
-    print("Here are the feminine coded words:")
-    print_fem(resume)
-    print("Here are the masculine coded words:")
-    print_masc(resume)
